14fbda117e563ab690bf71f19ce0893d775cf491
Cesar Gallardo <wuelo789@gmail.com>
2021-08-23 10:25:50 -0500

    Added NumberInput Component

---
+++ src/components/Input/Number/NumberInput.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+import './NumberInput.css';
+
+const NumberInput = ({id, src, imageAlt, label, onInputChangeHandler, value}) => {
+
+  const [active, setActive] = useState(false);
+
+  const onFocusHandler = () =>{
+    setActive(!active);
+  }
+
+  return (
+    <div style={{marginTop: "10px"}}>
+      <label htmlFor={id} className="number-input__label">{label}</label>
+      <div className="number-input">
+        <img className="number-input__image" src={src} alt={imageAlt} />
+        <input 
+          id={id}
+          name={id}
+          className="number-input__input"
+          onFocus={onFocusHandler}
+          onChange={onInputChangeHandler} 
+          value={value} 
+          type="number"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default NumberInput
