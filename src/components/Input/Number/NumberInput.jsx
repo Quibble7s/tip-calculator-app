64c93a55ab4ff55177e8aa5eac430692c7b19199
Cesar Gallardo <wuelo789@gmail.com>
2021-08-24 14:34:08 -0500

    Added a highlight when the input is focused

--- src/components/Input/Number/NumberInput.jsx
+++ src/components/Input/Number/NumberInput.jsx
@@ -1,26 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, {useState } from 'react'
 import Label from '../Label/Label';
 
 import './NumberInput.css';
 
 const NumberInput = ({id, src, imageAlt, label, onInputChangeHandler, value}) => {
 
-  const [active, setActive] = useState(false);
-
+  const [active, setActive] = useState("number-input");
   const onFocusHandler = () =>{
-    setActive(!active);
+    setActive("number-input number-input--active");
+  }
+  const onLostFocusHandler = () => {
+    setActive("number-input");
   }
 
   return (
     <div style={{marginTop: "10px"}}>
       <Label id={id} label={label}/>
-      <div className="number-input">
+      <div className={active}>
         <img className="number-input__image" src={src} alt={imageAlt} />
         <input 
           id={id}
           name={id}
           className="number-input__input"
           onFocus={onFocusHandler}
+          onBlur={onLostFocusHandler}
           onChange={onInputChangeHandler} 
           value={value} 
           type="number"
