cb2a1c7babb86403d3058220b4daf661705c7f2e
Cesar Gallardo <wuelo789@gmail.com>
2021-08-24 20:31:30 -0500

    changed from buttons to radio buttons

--- src/components/Input/PercentageSelector/PercentageSelector.jsx
+++ src/components/Input/PercentageSelector/PercentageSelector.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import Label from '../Label/Label'
+import Percentage from './Percentage/Percentage'
 import './PercentageSelector.css'
 
-const PercentageSelector = () => {
+const PercentageSelector = ({label, onClickHandler}) => {
+
   return (
     <div className="percentage-selector">
-      
+      <Label
+        label={label}
+      />
+      <div className="percentages">
+        <Percentage
+          id="0"
+          percentage="5%"
+          onSelected={onClickHandler}
+        />
+        <Percentage
+          id="1"
+          percentage="10%"
+          onSelected={onClickHandler}
+        />
+        <Percentage
+          id="2"
+          percentage="15%"
+          onSelected={onClickHandler}
+        />
+        <Percentage
+          id="3" 
+          percentage="25%"
+          onSelected={onClickHandler}
+        />
+        <Percentage
+          id="4"
+          percentage="50%"
+          onSelected={onClickHandler}
+        />
+      </div>
     </div>
   )
 }
